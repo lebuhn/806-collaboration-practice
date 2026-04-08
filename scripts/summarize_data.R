6966edd5e0d80c6f8e6009b932afe8592589b44d
campliata <lavender_poetry@hotmail.com>
2026-04-08 13:20:55 -0700

    added comment lines for issue #4

--- scripts/summarize_data.R
+++ scripts/summarize_data.R
@@ -1,7 +1,7 @@
-# Load data
+# Load data from scpes_count.csv
 data <- read.csv("data/species_counts.csv")
 
-# Calculate total count
+# Calculate total count by summing the data column and save it as variable total
 total <- sum(data$count)
 
 # Print result
